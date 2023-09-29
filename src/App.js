5d7b282f318a25b51fad48eda536239b6ea2795a
MGonzalesZ <mauriciogzb1322@gmail.com>
2023-09-29 14:58:05 -0400

    componentes ExpenseItem y Fecha

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+import Fecha from './components/Fecha';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+        <h2>GRUPO SISTEMAS</h2>
+        <ExpenseItem></ExpenseItem>
+        <Fecha></Fecha></div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
