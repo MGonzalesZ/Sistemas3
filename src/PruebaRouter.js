a4bcdc0fa27d45c8a6de39146d38408c6f368d77
MGonzalesZ <mauriciogzb1322@gmail.com>
2023-10-08 15:33:30 -0400

    Archivo pruebaRouter.js

--- src/PruebaRouter.js
+++ src/PruebaRouter.js
@@ -4,21 +4,22 @@ import DeporteItem from './components/MGonzalesZ/DeporteItem';
 import App from './App';
 import Canchas from './components/MGonzalesZ/Canchas';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function PruebaRouter() {
-    return (
-        <>
-        <Router>
-         <Routes>            
-            <Route path="/Sistemas3" element={<Canchas/>} />
-            <Route path="/deporte" element={<DeporteItem />} />
-          </Routes>
-        </Router>
-        </>
-    );
+  return (
+    <>
+      <Router>
+        <Routes>
+          {/* MGonzalesZ */}
+          <Route path="/Sistemas3" element={<Canchas />} />
+          <Route path="/deporte" element={<DeporteItem />} />
+          {/* Fermin */}
+          {/* Kael */}
+          {/* Mauricio Garcia */}
+          {/* Rene */}
+        </Routes>
+      </Router>
+    </>
+  );
 }
--- src/components/MGonzalesZ/Canchas.js
+++ src/components/MGonzalesZ/Canchas.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import './Canchas.css';
 import deportesImg from './img/deportes.jpg';
 import futsalImg from './img/futsal.jpg';
 import volleyImg from './img/volley.jpg';
 import basquetImg from './img/basquet.jpg';
-
+import Card from '../UI/Card';
 import CanchasAccesos from './CanchasAccesos';
 
 const Canchas = () => {
-
   const initial_deportes = [
     {
       id: 'd1',
@@ -28,71 +27,73 @@ const Canchas = () => {
     },
   ];
   return (
-    <div className="canchas">
-      <br/>
-      <h2>CANCHAS</h2>
-      <div className="row">
-        <div className="col-1"></div>
-        <div className="col-4">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui id
-            ornare arcu odio ut sem nulla pharetra. At auctor urna nunc id
-            cursus. Nulla aliquet porttitor lacus luctus accumsan. Arcu odio ut
-            sem nulla pharetra diam sit. Dui id ornare arcu odio ut. Ornare
-            suspendisse sed nisi lacus sed. Adipiscing tristique risus nec
-            feugiat in fermentum posuere urna nec. Pharetra et ultrices neque
-            ornare aenean. Mus mauris vitae ultricies leo integer malesuada nunc
-            vel. Ut aliquam purus sit amet. At quis risus sed vulputate odio ut
-            enim blandit. Et leo duis ut diam quam. Tempus urna et pharetra
-            pharetra massa massa ultricies mi quis. Et leo duis ut diam quam
-            nulla. Eu non diam phasellus vestibulum. Habitasse platea dictumst
-            quisque sagittis purus sit. Praesent semper feugiat nibh sed
-            pulvinar proin gravida hendrerit lectus. Pulvinar etiam non quam
-            lacus. Sit amet facilisis magna etiam tempor orci. Nunc sed velit
-            dignissim sodales ut eu. Tempor id eu nisl nunc mi ipsum. Dignissim
-            suspendisse in est ante in nibh mauris. Aenean vel elit scelerisque
-            mauris pellentesque pulvinar pellentesque. Vestibulum morbi blandit
-            cursus risus at ultrices mi tempus imperdiet. Sit amet justo donec
-            enim diam. Elit sed vulputate mi sit. Blandit massa enim nec dui
-            nunc.
-          </p>
-        </div>
-        <div className="col-1"></div>
-        <div className="col-4">
-          <img src={deportesImg} width="100%" alt="Deportes" />
-        </div>
-        <div className="col-1"></div>
-      </div>
-      <div className="row">
-        <h3>Deportes</h3>
+    <Card>
+      <div className="canchas">
         <br />
-        <br />
+        <h2>CANCHAS</h2>
+        <div className="row">
+          <div className="col-1"></div>
+          <div className="col-4">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui id
+              ornare arcu odio ut sem nulla pharetra. At auctor urna nunc id
+              cursus. Nulla aliquet porttitor lacus luctus accumsan. Arcu odio
+              ut sem nulla pharetra diam sit. Dui id ornare arcu odio ut. Ornare
+              suspendisse sed nisi lacus sed. Adipiscing tristique risus nec
+              feugiat in fermentum posuere urna nec. Pharetra et ultrices neque
+              ornare aenean. Mus mauris vitae ultricies leo integer malesuada
+              nunc vel. Ut aliquam purus sit amet. At quis risus sed vulputate
+              odio ut enim blandit. Et leo duis ut diam quam. Tempus urna et
+              pharetra pharetra massa massa ultricies mi quis. Et leo duis ut
+              diam quam nulla. Eu non diam phasellus vestibulum. Habitasse
+              platea dictumst quisque sagittis purus sit. Praesent semper
+              feugiat nibh sed pulvinar proin gravida hendrerit lectus. Pulvinar
+              etiam non quam lacus. Sit amet facilisis magna etiam tempor orci.
+              Nunc sed velit dignissim sodales ut eu. Tempor id eu nisl nunc mi
+              ipsum. Dignissim suspendisse in est ante in nibh mauris. Aenean
+              vel elit scelerisque mauris pellentesque pulvinar pellentesque.
+              Vestibulum morbi blandit cursus risus at ultrices mi tempus
+              imperdiet. Sit amet justo donec enim diam. Elit sed vulputate mi
+              sit. Blandit massa enim nec dui nunc.
+            </p>
+          </div>
+          <div className="col-1"></div>
+          <div className="col-4">
+            <img src={deportesImg} width="100%" alt="Deportes" />
+          </div>
+          <div className="col-1"></div>
+        </div>
+        <div className="row">
+          <h3>Deportes</h3>
+          <br />
+          <br />
+        </div>
+        <div className="row">
+          <div className="col-1"></div>
+          <div className="col-2 deporte-item">
+            <Link to="/deporte">
+              <h4>{initial_deportes[0].titulo}</h4>
+              <img src={futsalImg} alt={initial_deportes[0].titulo} />
+            </Link>
+          </div>
+          <div className="col-2"></div>
+
+          <div className="col-2 deporte-item">
+            <h4>{initial_deportes[1].titulo}</h4>
+
+            <img src={volleyImg} alt={initial_deportes[1].titulo} />
+          </div>
+          <div className="col-2"></div>
+          <div className="col-2 deporte-item">
+            <h4>{initial_deportes[2].titulo}</h4>
+
+            <img src={basquetImg} alt={initial_deportes[2].titulo} />
+          </div>
+          <div className="col-1"></div>
+        </div>
       </div>
-      <div className="row">
-        <div className="col-1"></div>
-        <div className="col-2 deporte-item">
-        <Link to="/deporte">          
-            <h4>{initial_deportes[0].titulo}</h4>
-            <img src={futsalImg} alt={initial_deportes[0].titulo} />          
-        </Link>
-        </div>
-        <div className="col-2"></div>
-
-        <div className="col-2 deporte-item">
-          <h4>{initial_deportes[1].titulo}</h4>
-
-          <img src={volleyImg} alt={initial_deportes[1].titulo} />
-        </div>
-        <div className="col-2"></div>
-        <div className="col-2 deporte-item">
-          <h4>{initial_deportes[2].titulo}</h4>
-
-          <img src={basquetImg} alt={initial_deportes[2].titulo} />
-        </div>
-        <div className="col-1"></div>
-      </div>
-    </div>
+    </Card>
   );
 };
 export default Canchas;
