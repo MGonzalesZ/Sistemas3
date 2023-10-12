803de1acf78df3693fa9d36afa813f1fd0c3ea56
fermin becerra <95020613+fergta54@users.noreply.github.com>
2023-10-12 13:34:09 -0400

    Imagenes Update
    Amongus

--- src/components/PlataformaAtencion/Plataforma.js
+++ src/components/PlataformaAtencion/Plataforma.js
@@ -31,44 +31,44 @@ function Plataforma() {
         titulo="Bienvenido"
         desc="Estamos encantados de darte la bienvenida a nuestro espacio virtual diseñado exclusivamente para satisfacer tus necesidades académicas y brindarte el apoyo que necesitas durante tu trayecto educativo."
         nombre="Ir al siu"
-        src="https://hesolutions.com.pk/wp-content/uploads/2019/01/picture-not-available.jpg"
+        src="https://framerusercontent.com/images/6pVuQvX6xdB3XLndYeXPpdKeA.png?scale-down-to=512"
       />
 
       <C2l
         titulo="Plan de Pagos"
         desc="Descubre nuestro flexible plan de pagos universitarios. ¡Educa sin preocupaciones!"
         nombre="Ver mas"
-        image="https://hesolutions.com.pk/wp-content/uploads/2019/01/picture-not-available.jpg"
+        image="https://framerusercontent.com/images/4Kjnpu4oOCA0oWLpsBVPYrODRE.jpg?scale-down-to=512"
       />
       <C2r
         titulo="Inscripciones"
         desc="Inscripciones abiertas: ¡Prepárate para tu futuro académico en Univalle"
         nombre="Ver mas"
-        image="https://hesolutions.com.pk/wp-content/uploads/2019/01/picture-not-available.jpg"
+        image="https://framerusercontent.com/images/EjZ9HQTX6LKDLu6DTh9qo9o8kM.png?scale-down-to=512"
       />
       <C2l
         titulo="Carreras"
         desc="Explora nuestras emocionantes carreras universitarias. ¡Elige tu camino!"
         nombre="Ver mas"
-        image="https://hesolutions.com.pk/wp-content/uploads/2019/01/picture-not-available.jpg"
+        image="https://framerusercontent.com/images/jkzNytuMjsnRfaMeEhatziN6cw.jpg?scale-down-to=512"
       />
       <C2r
         titulo="Credenciales"
         desc="Consigue tus credenciales de entrada a Univalle hoy mismo."
         nombre="Ver mas"
-        image="https://hesolutions.com.pk/wp-content/uploads/2019/01/picture-not-available.jpg"
+        image="https://framerusercontent.com/images/owsUEqPT5m4aFgBeVv6AlTQfHQ.jpg"
       />
       <C2l
         titulo="Solvencia"
         desc="Asegura tu solvencia en Univalle Tu inversión en el futuro."
         nombre="Ver mas"
-        image="https://hesolutions.com.pk/wp-content/uploads/2019/01/picture-not-available.jpg"
+        image="https://framerusercontent.com/images/kB3vRDR3ICFqHC9sQGS3mkcNsY.jpg?scale-down-to=512"
       />
       <C2r
         titulo="Horarios"
         desc="Revisa los horarios para visitarnos."
         nombre="Ver mas"
-        image="https://hesolutions.com.pk/wp-content/uploads/2019/01/picture-not-available.jpg"
+        image="https://framerusercontent.com/images/k85UjfKkov66wjwAAkSMOYlxFQ.jpg?scale-down-to=512"
       />
 
       <Titulo titulo="Contactos" fontstyle={fstyle2} />
